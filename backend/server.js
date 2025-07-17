7734cf99f5aced4b852c244d6ed0fdbd25faba47
JANGDONGMIN <jjangdm@gmail.com>
2025-07-18 03:51:46 +0900

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const Student = require('./models/student');
 const Book = require('./models/book');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // 환경별 설정
 const isDevelopment = process.env.NODE_ENV !== 'production';
